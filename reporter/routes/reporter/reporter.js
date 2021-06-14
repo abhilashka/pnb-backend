16c144e676980702c12bf821c8b8036f70e7e7d9
cyrilsibi99 <83388865+cyrilsibi99@users.noreply.github.com>
2021-06-14 19:08:31 +0530

    maxId used

--- reporter/routes/reporter/reporter.js
+++ reporter/routes/reporter/reporter.js
@@ -3,7 +3,6 @@ const utils = require('../../utils')
 const db = require('../../db')
 const crypto = require('crypto-js')
 const jwt = require('jsonwebtoken')
-const db = require('../../db')
 const config = require('../../config')
 const multer = require('multer')
 const upload = multer({ dest: 'images/' })
@@ -33,59 +32,91 @@ const router = express.Router()
 
 //sign up
 router.post('/signup', (request, response) => {
-
-  // const { firstName, lastName, email, password } = request.body
-
-  // const encryptedPassword = crypto.SHA256(password)
-
-
-  const htmlPath = path.join(__dirname, '/../../templates/send_activation_link.html')
-  console.log(htmlPath)
-  let body = '' + fs.readFileSync(htmlPath)
-
-  let email = "abhiwardha@gmail.com"
   // const statement1 = 'insert into address(city,localities,town,pincode) VALUES("pune","pune","pune",1234);'
-  // db.query(statement1, (error, data) => {
-  // })
+  
+  const { first_name,last_name,phone,email,password,city,localities,state,pincode,type} = request.body;
+  // const encryptedPass = crypto.SHA256(password);
 
-  // const statement2 = `insert into person_details(first_name,last_name,address,phone,email,password) values("john","rayn",(select id from address where pincode=1234),"909090","steven3@gmail.com","steven");`
-  // db.query(statement2, (error, data) => {
-  // })
+  const stm=`SELECT MAX(id) AS MaxId FROM address`;
+  var maxId;
+  db.query(stm,(error, data) => {
+    if (error) {
+      console.log(error)
+    }    
+    else {
+      console.log(data[0].MaxId);
+      maxId=data[0].MaxId;
+    }
+  })
 
+  const statement1 = `insert into address(city,localities,state,pincode) VALUES('${city}','${localities}','${state}','${pincode}');`
+  
 
-  // const statement = `insert into reporter (reporter_detail) values ((select id from person_details where email="steven3@gmail.com"))`
-  const statement = `select * from news;`
+  db.query(statement1, (error, data) => {
+
+  })
+  const statement2 = `insert into user_details(first_name,last_name,address_id,phone,email,TYPE) values('${first_name}','${last_name}',(select id from address where pincode='${maxId}'),'${phone}','${email}','${type}');`
+  db.query(statement2, (error, data) => {
+    if (error) {
+      console.log(error)
+    }
+    else {
+      console.log(data)
+
+    }
+
+  })
+  const statement = `insert into user_crdntl (user_id,passwd) values ((select id from user_details where email='${email}'),'${password}')`
   db.query(statement, (error, data) => {
     if (error) {
       response.send(utils.createError(error))
 
     }
     else {
-
-      mailer.sendEmail(email, 'Public News Board', body, (error, info) => {
-
-        response.send(utils.createSuccess(data))
-
-      })
+      response.send(utils.createSuccess(data));
 
 
     }
-
-    const { firstName, lastName, email, password } = request.body
-
-    const encryptedPassword = crypto.SHA256(password)
-
-    //change query accroding to our db 
-    const statement = `insert into admin (firstName, lastName, email, password) values (
-    '${firstName}', '${lastName}', '${email}', '${encryptedPassword}'
-  )`
-    db.query(statement, (error, data) => {
-      response.send(utils.createResult(error, data))
-
-    })
   })
+});
 
-})
+
+
+
+
+  // // const statement = `insert into reporter (reporter_detail) values ((select id from person_details where email="steven3@gmail.com"))`
+  // const statement = `select * from news;`
+  // db.query(statement, (error, data) => {
+  //   if (error) {
+  //     response.send(utils.createError(error))
+
+  //   }
+  //   else {
+
+  //     mailer.sendEmail(email, 'Public News Board', body, (error, info) => {
+
+  //       response.send(utils.createSuccess(data))
+
+  //     })
+
+
+    // }
+
+//     const { firstName, lastName, email, password } = request.body
+
+//     const encryptedPassword = crypto.SHA256(password)
+
+//     //change query accroding to our db 
+//     const statement = `insert into admin (firstName, lastName, email, password) values (
+//     '${firstName}', '${lastName}', '${email}', '${encryptedPassword}'
+//   )`
+//     db.query(statement, (error, data) => {
+//       response.send(utils.createResult(error, data))
+
+//     })
+//   })
+
+// })
 
 
 
