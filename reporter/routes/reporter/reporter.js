147eb8ea70fc26ed9c837c74c5ca17bd879cf4bc
abhilash <abhilash.kamble376@gmail.com>
2021-06-15 12:20:47 +0530

    signin fixed with jwt

--- reporter/routes/reporter/reporter.js
+++ reporter/routes/reporter/reporter.js
@@ -2,8 +2,8 @@ const express = require('express')
 const utils = require('../../utils')
 const db = require('../../db')
 const crypto = require('crypto-js')
-
-
+const jwt = require('jsonwebtoken')
+const config = require('../../config')
 
 const router = express.Router()
 
@@ -87,6 +87,30 @@ router.post('/signup', (request, response) => {
 
 
 
+//sign in
+router.post('/signin', (request, response) => {
+  const { email, password } = request.body
+  const statement = `select u.email,c.passwd from user_details u join user_crdntl c  on u.id=c.id where u.email ='${email}' and c.passwd='${password}';`
+
+
+  db.query(statement, (error, reporters) => {
+    if (error) {
+      response.send({ status: 'error', error: error })
+    } else {
+      if (reporters.length == 0) {
+        response.send({ status: 'error', error: 'reporter does not exist' })
+      } else {
+        const reporter = reporters[0]
+        const token = jwt.sign({ id: reporter['id'] }, config.secret)
+        response.send(utils.createResult(error, {
+          first_name: reporter['first_name'],
+          last_name: reporter['last_name'],
+          token: token
+        }))
+      }
+    }
+  })
+})
 
 
 
